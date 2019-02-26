37caef87bb1255e11eafb40618b14096f8cd9616
Blake Jensen <30006830@student.toiohomai.ac.nz>
2019-02-27 10:44:52 +1300

    yeet

---
+++ AppMyLife/AppMyLife/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMyLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+
+
+
+
+            Console.WriteLine("Enter age here please: "); //asks for you to enter your age
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please type out your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine(name + age);
+            Console.ReadLine();
+
+        }
+    }
+}
